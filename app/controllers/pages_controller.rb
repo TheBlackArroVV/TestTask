2d649696da9feb119b5d08e05f6d6d7027b4e948
TheBlackArroVV <theblackarrovv@gmail.com>
2018-03-01 13:30:57 +0200

    read from custom CSV

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,15 @@ class PagesController < ApplicationController
   require 'csv'
 
   def index
-    @file = CSV.open(Rails.root.join("session_history.csv")) #do |csv|
-      # csv.each do |c|
-        # pp c
-      # end
-    # end
+    if params[:pages] != nil
+      pp @file = CSV.open(params[:pages][:file].path)
+    else
+      original_file
+    end
+  end
+
+private
+  def original_file
+    @file = CSV.open(Rails.root.join("session_history.csv"))
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'pages/index'
+  post 'pages/index'
+  get 'pages/original_file'
+  post 'pages/original_file'
+  resources :pages
 
   root to: 'pages#index'
 
